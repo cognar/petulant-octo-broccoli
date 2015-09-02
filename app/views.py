100c15663aeb44326b27921c96ac16bd55c81e28
Cognar <connor.mccloskey@gmail.com>
2015-09-02 14:13:17 -0700

    missed in last commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from random import randint
+from SudSolve import sudoku as solveSud
 
 @app.route('/')
 @app.route('/index')
@@ -13,3 +14,14 @@ def index():
 @app.route('/fuck')
 def fuck():
     return 'fuck you'
+
+@app.route('/sudoku')
+def sudoku():
+    return render_template('sudoku.html',
+                            title='sudoku',
+                            n=9,
+			    backgroundColor = ['FFFFE1', 'C4C9BB'])
+@app.route('/sudoku', methods=['POST'])
+def solveSudoku():
+    numbers = ''.join([request.form[str(i)] for i in range(1,82)])
+    return solveSud(numbers)
